cb2d6c1d3c744e4e1b9ca33349c243438651733f
perpera <poalina@icloud.com>
2025-05-29 16:08:14 +0300

    updated README.md

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter basename="/movie-search-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
